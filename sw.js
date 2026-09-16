6edaea11db946ccdbac4f1ea7b32b2b994246e1e
fagnerjrtelecom-max <fagnerjrtelecom@gmail.com>
2026-09-16 09:20:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eletrostock-cache-v1';
+const CACHE_NAME = 'eletrostock-cache-v2';
 const urlsToCache = [
   './index.html',
   './manifest.json',
@@ -7,12 +7,33 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
+  // Ignora chamadas enviadas para o banco de dados do Supabase
+  if (event.request.url.includes('supabase.co')) {
+    return; // Deixa o navegador fazer a requisição HTTP real direto para a nuvem
+  }
+
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
